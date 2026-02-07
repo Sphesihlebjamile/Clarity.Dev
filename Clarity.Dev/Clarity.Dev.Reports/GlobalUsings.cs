67b577f920d3d310dbab442e2192405f9af83f9a
Sphesihlebjamile <165266044+Sphesihlebjamile@users.noreply.github.com>
2026-02-07 17:03:33 +0200

    [feat]: Created Clarity.Dev.Reports projects and added a HtmlReportGenerator template

---
+++ Clarity.Dev/Clarity.Dev.Reports/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Text;
+global using SolutionModels = Clarity.Dev.NET.Core.Models;
+
+namespace Clarity.Dev.Reports;
---
+++ Clarity.Dev/Clarity.Dev.Reports/HtmlReportGenerator.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Clarity.Dev.Reports;
+
+public class HtmlReportGenerator
+{
+    public string GenerateReport(SolutionModels.SolutionAnalysisResult result, string outputPath)
+    {
+        throw new NotImplementedException();
+    }
+
+    private string GenerateProjectDependencyDiagram(List<SolutionModels.ProjectInfo> projects)
+    {
+        throw new NotImplementedException();
+    }
+
+    private string GenerateServiceCommunicationDiagram(SolutionModels.SolutionAnalysisResult result)
+    {
+        throw new NotImplementedException();
+    }
+
+    private string GenerateProjectSection(SolutionModels.ProjectInfo project)
+    {
+        throw new NotImplementedException();
+    }
+}
