9d9fde3aeb418158a9663439ff6a5dee5a40753b
Sphesihlebjamile <165266044+Sphesihlebjamile@users.noreply.github.com>
2026-02-23 20:24:02 +0200

    [chore]: added the IConsoleService interfact

---
+++ Clarity.Dev/src/Clarity.Dev.CLI/Contracts/IConsoleService.cs
@@ -0,0 +1,15 @@
+﻿namespace Clarity.Dev.CLI.Contracts;
+
+public interface IConsoleService
+{
+    public void DisplayHeader(string version);
+    public void DisplayLineSeparator();
+    public void DisplayNewLine();
+    public void DisplayInfor(string message);
+    public void DisplaySuccess(string message);
+    public void DisplayWarning(string message);
+    public void DisplayError(string message);
+    public void DisplayErrorWithStackTrace(string message, string stackTrace);
+    public void setForegroundColor(ConsoleColor color);
+    public void ResetColor();
+}
